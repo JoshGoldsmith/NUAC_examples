ce6e36d706d7e38a1b9c292b14a02c7ac710dad5
Josh Goldsmith <joshgoldsmith5793@gmail.com>
2022-07-25 14:51:57 +0000

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,16 @@
+alphabet_map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+#this will be used to 'map' every letter to a number. This will make the encryption process easier.
+
+#text to be encrypted -- Your text has to be inside quotation marks
+plaintext = ''
+
+#how much to shift the text by
+key = 0
+
+
+#encrypt(string, integer) -> string
+def encrypt(pt, k):
+
+
+#call the function
+print(encrypt(plaintext, key))
